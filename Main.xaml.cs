91add0dc2d9441208b7aa8bec60e9ff27a06b81a
tianxincc <1065986093@qq.com>
2022-05-12 17:33:33 +0800

    一些DEMO

---
+++ Main.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace LetMeRemindYou
+{
+    /// <summary>
+    /// Main.xaml 的交互逻辑
+    /// </summary>
+    public partial class Main 
+    {
+        public Main()
+        {
+            InitializeComponent();
+            Loaded += NotifyWindow_Loaded;
+        }
+
+        private void NotifyWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            Left = SystemParameters.WorkArea.Right - this.Width;
+            Top = SystemParameters.WorkArea.Bottom;
+            var animation = new DoubleAnimation
+            {
+                Duration = new Duration(TimeSpan.FromSeconds(0.5)),
+                To = SystemParameters.WorkArea.Bottom - this.Height,
+            };
+            this.BeginAnimation(TopProperty, animation);
+        }
+    }
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,6 +1,11 @@
-﻿using System;
+﻿using HandyControl.Controls;
+using HandyControl.Tools.Extension;
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Reflection;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -12,16 +17,45 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+using MessageBox = HandyControl.Controls.MessageBox;
+
 namespace LetMeRemindYou
 {
     /// <summary>
     /// MainWindow.xaml 的交互逻辑
     /// </summary>
-    public partial class MainWindow 
+    public partial class MainWindow
     {
         public MainWindow()
         {
             InitializeComponent();
         }
+
+
+        private void FormSub_Click(object sender, RoutedEventArgs e)
+        {
+            Form form = new Form();
+            form.ShowDialog();
+        }
+
+        private void JingLingClick(object sender, RoutedEventArgs e)
+        {
+            Sprite.Show(new AppSprite());
+        }
+
+        private void YiYan_Click(object sender, RoutedEventArgs e)
+        {
+            WebClient MyWebClient = new WebClient();
+            MyWebClient.Credentials = CredentialCache.DefaultCredentials;
+            Byte[] pageData = MyWebClient.DownloadData("https://v1.hitokoto.cn");
+            string pageHtml = Encoding.UTF8.GetString(pageData);
+            //PopupWindow.ShowDialog(pageHtml,"一言",false);
+            //PopupWindow.Show(pageHtml, true);
+            //Growl.Success(pageHtml);
+        }
+
+       
+
+
     }
 }
